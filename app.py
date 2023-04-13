f63150866444bea73f32af9f90da7e299695c879
luthfi sangaji <luthfisangaji2301@gmail.com>
2023-04-13 11:18:21 +0700

    add

---
+++ app.py
@@ -0,0 +1,108 @@
+from ast import Try
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
+# from services import sessionService, voucherService, regionService
+import os
+  
+  
+app = Flask(__name__)
+api = Api(app)
+
+app.secret_key = 'xyzsdfg'
+app.config['JSON_SORT_KEYS'] = False
+
+  
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_DB'] = 'skripsi'
+  
+mysql = MySQL(app)
+  
+@app.route('/')
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    mesage = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        result = {}
+        email = request.form['email']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM account WHERE email = % s AND password = % s', (email, password, ))
+        user = cursor.fetchone()
+        if user:
+            result['loggedin'] = True
+            result['id'] = user['id']
+            result['username'] = user['username']
+            result['email'] = user['email']
+            mesage = 'Logged in successfully !'
+            # return render_template('user.html', mesage = mesage)
+
+        else:
+            mesage = 'Please enter correct email / password !'
+    # return render_template('login.html', mesage = mesage)
+        result['mesage'] = mesage
+    return jsonify(result)
+  
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('email', None)
+    return redirect(url_for('login'))
+  
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    mesage = ''
+    
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+        result = {}
+        userName = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM account WHERE email = % s', (email, ))
+        account = cursor.fetchone()
+        if account:
+            mesage = 'Account already exists !'
+            resp = '99'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            mesage = 'Invalid email address !'
+            resp = '99'
+        elif not userName or not password or not email:
+            mesage = 'Please fill out the form !'
+            resp = '99'
+        else:
+            cursor.execute('INSERT INTO account VALUES (NULL, % s, % s, % s)', (userName, password,email,))
+            # status, result= cursor.executeData('INSERT INTO account VALUES (NULL, % s, % s, % s)', (userName, password,email,))
+            # print(result)
+            # print(status)
+            mysql.connection.commit()
+            mesage = 'You have successfully registered !'
+            resp = '00'
+    elif request.method == 'POST':
+        mesage = 'Please fill out the form !'
+    # return 'username': userName
+    result['response_message'] = mesage
+    result['response_code'] = resp
+    return jsonify(result)
+    # return render_template('register.html', mesage = mesage)
+
+# class App(Resource):
+    # def post(self,serviceName):
+    #     try:
+    #         # rs = self.rs
+    #         if serviceName =='register':
+    #             print('iok')
+    #             username = request.headers.get('username', None)
+    #             self['data'].append(username)
+    #         return jsonify(self)
+    #     except Exception as e:
+    #         # common.write_log_error(str(e))
+    #         return jsonify({'Error': str(e)})
+# api.add_resource(App, '/<string:serviceName>')
+if __name__ == "__main__":
+    app.run()
